41fbe41416131f8263501c7d4020446ca09f6588
sejkim2 <kimsejin2118@gmail.com>
2025-04-27 18:49:33 +0900

    add : exception 한번에 처리

---
+++ requirements/backend/app/src/main/java/com/example/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());  // 예: "이미 존재하는 이메일입니다."
+    }
+}
