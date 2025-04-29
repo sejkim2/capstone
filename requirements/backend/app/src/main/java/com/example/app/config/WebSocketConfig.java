b8fe0b89c38eafe51a34daef1c520f7eefde4dec
sejkim2 <kimsejin2118@gmail.com>
2025-04-29 22:13:28 +0900

    fix : websocket buffer size -> 300kb

--- requirements/backend/app/src/main/java/com/example/app/config/WebSocketConfig.java
+++ requirements/backend/app/src/main/java/com/example/app/config/WebSocketConfig.java
@@ -2,8 +2,12 @@ package com.example.app.config;
 
 import com.example.app.frame.FrameWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
 
 @Configuration
 @EnableWebSocket
@@ -16,5 +20,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(frameWebSocketHandler, "/ws/frame")
                 .setAllowedOrigins("*"); // 개발용 허용
+    }
+
+    // WebSocket 버퍼 크기 300KB 설정
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxBinaryMessageBufferSize(300 * 1024); // 300KB
+        container.setMaxTextMessageBufferSize(300 * 1024);   // 300KB
+        return container;
     }
 }
